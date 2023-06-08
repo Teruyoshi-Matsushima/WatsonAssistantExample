a4a87fac04422478799dbd110f616cac5a28fee3
Teruyoshi Matsushima <73378281+Teruyoshi-Matsushima@users.noreply.github.com>
2023-06-08 13:55:07 +0900

    Add files via upload

--- watson_send_message_1.py
+++ watson_send_message_1.py
@@ -3,17 +3,17 @@ from ibm_watson import AssistantV2
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 # Create Assistant service object.
-authenticator = IAMAuthenticator('KeeDBZ2zSdrDxeBgLEiOjXofCmyh1OPpj23y9IYJRB63') # replace with API key  資格情報
+authenticator = IAMAuthenticator('xxxxxxxxxxxxxxxxxx') # replace with API key  資格情報
 assistant = AssistantV2(
     version = '2023-05-20',
     authenticator = authenticator
 )
-assistant.set_service_url('https://api.jp-tok.assistant.watson.cloud.ibm.com/instances/4a935a11-2fdf-4d93-bde7-444776ec1052') # replace with service instance URL  資格情報
-assistant_id = '04361d3b-f463-43ef-8ed4-93cb47c0de16' # replace with environment ID
+assistant.set_service_url('https://xxxxxxxxxxxxxxxxxx') # replace with service instance URL  資格情報
+assistant_id = 'xxxxxxxxxxxxxxxxxx' # replace with environment ID
  
 response = assistant.message(
     assistant_id, # replace with environment ID
-    session_id = '7dad8261-37ab-46b5-aae2-aaa24a2fa96b',
+    session_id = 'xxxxxxxxxxxxxxxxxx',
     input={
         'message_type': 'text',
         'text': '天気を教えて',
